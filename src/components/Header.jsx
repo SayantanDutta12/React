c5dd5b71e92cb3acdf717acdaefbd14759e4eedc
SayantanDutta12 <82900345+SayantanDutta12@users.noreply.github.com>
2023-07-15 22:17:51 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <nav>
             <h1>TechStar</h1>
             <main>
-                <HashLink to={"/#home"}>Home</HashLink>
+                <HashLink to={"/React"}>Home</HashLink>
                 <Link to={"/contact"}>Contact</Link>
                 <HashLink to={"/#about"}>About</HashLink>
                 <HashLink to={"/#brands"}>Brands</HashLink>
